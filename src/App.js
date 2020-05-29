e51efeaf6fafa9fcc9321eb20d858073d5de39c3
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-05-29 09:47:39 -0700

    modify app.js file

--- src/App.js
+++ src/App.js
@@ -1,158 +1,26 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  ListItemSecondaryAction,
-  ListItemAvatar,
-  ListItemIcon,
-  Avatar,
-  TextField,
-  IconButton,
-  Container,
-  Paper
-} from "@material-ui/core";
-import { green } from "@material-ui/core/colors";
-import AssignmentIcon from "@material-ui/icons/Assignment";
-import DeleteIcon from "@material-ui/icons/Delete";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const useStyles = makeStyles({
-  greenAvatar: {
-    margin: 10,
-    color: "#fff",
-    backgroundColor: green[500]
-  }
-});
-
-function TodoList({ todo, index, completeTodo, deleteTodo }) {
-  const classes = useStyles();
-
+function App() {
   return (
-    <List>
-      <ListItem
-        button
-        onClick={() => completeTodo(index)}
-        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-      >
-        <ListItemAvatar>
-          <Avatar className={classes.greenAvatar}>
-            <AssignmentIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={todo.text} />
-        <ListItemSecondaryAction>
-          <ListItemIcon>
-            <IconButton
-              edge="end"
-              aria-label="delete"
-              onClick={() => deleteTodo(index)}
-            >
-              <DeleteIcon />
-            </IconButton>
-          </ListItemIcon>
-        </ListItemSecondaryAction>
-      </ListItem>
-    </List>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello
+        </a>
+      </header>
+    </div>
   );
 }
 
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        id="standard-bare"
-        margin="normal"
-        value={value}
-        placeholder="Add Todo..."
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
-
-export default function TodoApp() {
-  const [todos, setTodos] = useState([
-    {
-      text: "Practice JavaScript",
-      isCompleted: false
-    },
-    {
-      text: "Update Resume",
-      isCompleted: false
-    },
-    {
-      text: "Go Kyaking",
-      isCompleted: false
-    }
-  ]);
-
-  const addTodo = text => {
-    const newTodos = [{ text }, ...todos];
-    setTodos(newTodos);
-  };
-
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = !todos[index].isCompleted;
-    setTodos(newTodos);
-  };
-
-  const deleteTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
-  return (
-    <Container maxWidth="md">
-      <TodoForm addTodo={addTodo} />
-      <Paper>
-        {todos.map((todo, index) => (
-          <TodoList
-            key={index}
-            index={index}
-            todo={todo}
-            completeTodo={completeTodo}
-            deleteTodo={deleteTodo}
-          />
-        ))}
-      </Paper>
-    </Container>
-  );
-}
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Hello
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
