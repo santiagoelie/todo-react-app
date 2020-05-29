9f3fc2e45d5c42783158bef166365a794b57c7fb
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-05-29 10:28:12 -0700

    Updates

---
+++ precache-manifest.ba0ac7bd974451bc0f18a0c337602659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e477123852d3d24d3cd536c109f925c",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "94b934d7bf2a2a7b6ea7",
+    "url": "/todo-react-app/static/css/main.df93c76c.chunk.css"
+  },
+  {
+    "revision": "9ff79afea7d7f615c37f",
+    "url": "/todo-react-app/static/js/2.e298381b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-react-app/static/js/2.e298381b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b934d7bf2a2a7b6ea7",
+    "url": "/todo-react-app/static/js/main.52f43075.chunk.js"
+  },
+  {
+    "revision": "0cca69ce0c962285eded",
+    "url": "/todo-react-app/static/js/runtime-main.f962ee76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
